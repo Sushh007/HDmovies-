a90fb6bf91ea8c841b200a1aa1b85a46fa9c71a3
Sushh007 <160040312+Sushh007@users.noreply.github.com>
2024-07-21 22:14:27 +0530

    Add files via upload

--- api/apiConfig.js
+++ api/apiConfig.js
@@ -1,8 +1,8 @@
 const apiConfig = {
-    baseUrl: 'https://api.themoviedb.org/3/',
-    apiKey: '79c40e61e2e0398f72eadfe7d610ab8b',
+    baseUrl: '/3/movie/11',
+    apikey: '79c40e61e2e0398f72eadfe7d610ab8b',
     originalImage: (imgPath) => `https://image.tmdb.org/t/p/original/${imgPath}`,
-    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500/${imgPath}`,
-};
+    w500Image: (imgPath) => `https://image.tmdb.org/t/p/w500${imgPath}`
+}
 
 export default apiConfig;
